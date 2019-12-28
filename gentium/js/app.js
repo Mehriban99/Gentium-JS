84e07f7b8a529160a0aaa7904d3b4f9eec4cf20e
Mehriban99 <57294601+Mehriban99@users.noreply.github.com>
2019-12-28 21:30:20 +0400

    Add files via upload

---
+++ gentium/js/app.js
@@ -0,0 +1,19 @@
+function MyFunction1() {
+    var UserName = document.getElementById("sidebar")
+    if(UserName.style.display = "none") {
+        UserName.style.display = "block"
+    }
+    else{
+        UserName.style.display = "none"
+    }
+}
+
+function MyFunction2() {
+    var UserName = document.getElementById("sidebar")
+    if(UserName.style.display = "block") {
+        UserName.style.display = "none"
+    }
+    else{
+        UserName.style.display = "block"
+    }
+}
